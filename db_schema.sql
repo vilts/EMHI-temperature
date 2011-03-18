c04fde5069e4066dfa57ad567c14b8f39a7968ff
Viljo Marrandi <viljo@marrandi.ee>
2011-03-18 16:48:44 +0200

    Regexps to DB, possibility to get temp for many cities

--- db_schema.sql
+++ db_schema.sql
@@ -1,5 +1,16 @@
 CREATE TABLE emhi (
     id INTEGER PRIMARY KEY,
     temperature REAL,
+    city_id INTEGER,
     timestamp TEXT
 );
+
+CREATE TABLE cities (
+    id INTEGER PRIMARY KEY,
+    regexp TEXT,
+    name TEXT
+);
+
+INSERT INTO cities (regexp, name) VALUES ('<div style="position: absolute; left: 247px; top: 82px; width:70px; height:13px; color:#993300;" class="kaarditekst">(-?\d+\.?\d+)</div>', 'Tallinn');
+INSERT INTO cities (regexp, name) VALUES ('<div style="position: absolute; left: 322px; top: 187px; width:70px; height:13px; color:#993300;" class="kaarditekst">(-?\d+\.?\d+)</div>', 'Türi');
+INSERT INTO cities (regexp, name) VALUES ('<div style="position: absolute; left: 418px; top: 279px; width:70px; height:13px; color:#993300;" class="kaarditekst">(-?\d+\.?\d+)</div>', 'Tartu');
--- emhi.py
+++ emhi.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python                                                                                                                                                                                                                                     
-# -*- coding: utf-8                                                                                                                                                                                                                                   
-
+# -*- coding: utf-8                                                                                                                                                                                                                                 
 import httplib
 import re
 from time import gmtime, strftime
 import sqlite3
 
-filepath = '/home/vilts/'
+filepath = '/home/vilts/devel/EMHI-temperature/'
 logfile_name = 'emhi_temp.log'
 tempfile_name = 'emhi_temp.txt'
 dbname = 'emhi.db'
 
 dbconn = sqlite3.connect(filepath + dbname)
+dbconn.row_factory = sqlite3.Row
 
 def last_db_temp():
     cur = dbconn.cursor()
@@ -28,25 +28,38 @@ def insert_db_temp(degC):
     cur.close()
 
 conn = httplib.HTTPConnection('www.emhi.ee')
-conn.request("GET", "/")
+conn.request("GET", "/?ide=21")
 resp = conn.getresponse()
 weather_data = resp.read()
 
-matchObj = re.search( r'<div style="position: absolute; left: 179px; top: 62px; width:37px; height:11px; color:#FFFFFF; background-color:#003399; text-align: center;" class="mintavatext">(-?\d+\.?\d+).?C</div>', weather_data, re.M)
-degC = float(matchObj.group(1))
+# Get all cities and their regexps from DB
+cur = dbconn.cursor()
+cur.execute("SELECT * FROM cities");
+for row in cur:
+    pattern = re.compile(row["regexp"])
+    city_obj = re.search(pattern, weather_data)
+    city_temp = float(city_obj.group(1))
+    print u"[{0}] {1}".format(row["name"], city_temp)
 
-if matchObj:
+tallinn_obj = re.search( r'<div style="position: absolute; left: 247px; top: 82px; width:70px; height:13px; color:#993300;" class="kaarditekst">(-?\d+\.?\d+)</div>', weather_data)
+tallinn_temp = float(tallinn_obj.group(1))
+
+tyri_obj = re.search( r'<div style="position: absolute; left: 322px; top: 187px; width:70px; height:13px; color:#993300;" class="kaarditekst">(-?\d+\.?\d+)</div>', weather_data)
+tyri_temp = float(tyri_obj.group(1))
+
+if tallinn_obj and tyri_obj:
     current_time = strftime("%d/%m/%Y %H:%M:%S", gmtime())
-    print "[{0}]: {1}".format(current_time, degC)
+    print "[{0}]: {1}".format(current_time, tallinn_temp)
+    print "[{0}]: {1}".format(current_time, tyri_temp)
 else:
     print "Didn't find temperature in web page."
 
-old_temp = last_db_temp()
+# old_temp = last_db_temp()
 
-if old_temp != degC:
-    print "Inserting new temp to DB: " + str(degC)
-    insert_db_temp(degC)
-else:
-    print "Old temp matches current: " + str(old_temp) + " = " + str(degC)
+#if old_temp != degC:
+#    print "Inserting new temp to DB: " + str(degC)
+#    insert_db_temp(degC)
+#else:
+#    print "Old temp matches current: " + str(old_temp) + " = " + str(degC)
 
 dbconn.close()
