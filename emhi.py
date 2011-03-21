03ecc7821691cb7a9b639ae308ad7cb3d21bcaa3
Viljo Marrandi <viljo@marrandi.ee>
2011-03-21 16:15:12 +0200

    Init functionality, getopt start

--- emhi.py
+++ emhi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python                                                                                                                                                                                                                                     
 # -*- coding: utf-8                                                                                                                                                                                                                                 
-import httplib, re, sqlite3, sys
+import httplib, re, sqlite3, sys, getopt
 from time import gmtime, strftime
 
 filepath = '/home/vilts/devel/EMHI-temperature/'
@@ -8,6 +8,44 @@ dbname = 'emhi.db'
 
 dbconn = sqlite3.connect(filepath + dbname)
 dbconn.row_factory = sqlite3.Row
+init_db = 0
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    conn = httplib.HTTPConnection('www.emhi.ee')
+    conn.request("GET", "/?ide=21")
+    resp = conn.getresponse()
+    weather_data = resp.read()
+
+    # Get all cities and their regexps from DB
+    cur = dbconn.cursor()
+    cur.execute("SELECT * FROM cities");
+    for city in cur:
+        current_time = strftime("%d/%m/%Y %H:%M:%S", gmtime())
+        pattern = re.compile(city["regexp"])
+        city_obj = re.search(pattern, weather_data)
+        city_temp = float(city_obj.group(1))
+        print u"[{0}] ({1}) {2} °C".format(current_time, city["name"], city_temp)
+        if init_db:
+            insert_db_temp(city["id"], city_temp)
+
+# old_temp = last_db_temp()
+            
+#if old_temp != degC:
+#    print "Inserting new temp to DB: " + str(degC)
+#    insert_db_temp(degC)
+#else:
+#    print "Old temp matches current: " + str(old_temp) + " = " + str(degC)
+
+dbconn.close()
+
 
 def last_db_temp():
     cur = dbconn.cursor()
@@ -16,34 +54,12 @@ def last_db_temp():
     cur.close()
     return row[0]
 
-def insert_db_temp(degC):
+def insert_db_temp(city_id, degC):
     cur = dbconn.cursor()
     print "Inserting to DB"
-    cur.execute("INSERT INTO emhi VALUES(NULL, " + str(degC) + ", datetime('now'))")
+    cur.execute("INSERT INTO emhi (city_id, temperature, timestamp) VALUES(" + str(city_id) + ", " + str(degC) + ", datetime('now'))")
     dbconn.commit()
     cur.close()
 
-conn = httplib.HTTPConnection('www.emhi.ee')
-conn.request("GET", "/?ide=21")
-resp = conn.getresponse()
-weather_data = resp.read()
-
-# Get all cities and their regexps from DB
-cur = dbconn.cursor()
-cur.execute("SELECT * FROM cities");
-for row in cur:
-    current_time = strftime("%d/%m/%Y %H:%M:%S", gmtime())
-    pattern = re.compile(row["regexp"])
-    city_obj = re.search(pattern, weather_data)
-    city_temp = float(city_obj.group(1))
-    print u"[{0}] ({1}) {2} °C".format(current_time, row["name"], city_temp)
-
-# old_temp = last_db_temp()
-
-#if old_temp != degC:
-#    print "Inserting new temp to DB: " + str(degC)
-#    insert_db_temp(degC)
-#else:
-#    print "Old temp matches current: " + str(old_temp) + " = " + str(degC)
-
-dbconn.close()
+if __name__ == "__main__":
+    main()
